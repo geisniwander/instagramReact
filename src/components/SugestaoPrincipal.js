99d34eeb7bb68497c6308b9fb431c4a30b4ed176
geisniwander <geisniwander@gmail.com>
2022-11-19 14:44:49 -0300

    "image and username in sidebar - changing dynamically"

--- src/components/SugestaoPrincipal.js
+++ src/components/SugestaoPrincipal.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function SugestaoPrincipal(props) {
-  return (
+  const [nome,setNome]=useState(props.nomePerfil)
+  const [imagem,setImagem]=useState(props.imagemPerfil)
+
+  function verificaUrl(str){
+        try {
+          new URL(str);
+          setImagem(str);
+        } catch (err) {
+          alert("URL inválida!")
+        }
+  }
+
+  function verificaNome(str){
+    if(!str) alert("Nome inválido!");
+    else setNome(str);
+  }
+
+    return (
     <div class="usuario">
-      <img src={props.imagemPerfil} />
+      <img src={imagem} onClick={()=>verificaUrl(prompt("Insira uma nova url de imagem"))} />
       <div class="texto">
         <strong>{props.usuarioPerfil}</strong>
         <span>
-          {props.nomePerfil}
-          <ion-icon name="pencil"></ion-icon>
+          {nome}
+          <ion-icon name="pencil" onClick={()=>verificaNome(prompt("Digite um nome de usuário"))}></ion-icon>
         </span>
       </div>
     </div>
